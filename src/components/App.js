ba714f0c40abba2a4b2e4f14ba65e0e7e6c0ad57
Paulo <paulonova144@gmail.com>
2020-12-18 14:45:44 +0100

    Search Form

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 
 class App extends React.Component{
 
   render(){
     return(
-      <div>App</div>
+      <div className="ui container">
+       <SearchBar/>
+      </div>
     );
   };
 }
